0eefe5cf71ff33126e87f91a87ac12c6c735b1c7
x-Retr0 <119910040+x-Retr0@users.noreply.github.com>
2023-03-12 20:55:41 +0100

    the info buttons were added

--- Rotating Navigation/app.js
+++ Rotating Navigation/app.js
@@ -1,6 +1,7 @@
 const square = document.getElementById("square");
 const circle = document.getElementById("circle");
 const menu = document.getElementById("menu");
+const info = document.getElementById("info");
 let action = true;
 let cooldown = false;
 
@@ -10,6 +11,8 @@ function activateRotation() {
     square.classList.toggle("rotate-inverse", !action);
     circle.classList.toggle("rotate", action);
     circle.classList.toggle("rotate-inverse", !action);
+    info.classList.toggle("entering", action);
+    info.classList.toggle("entering-inverse", !action);
     action = !action;
     cooldown = !cooldown;
     setTimeout(() => {
